3a7ff9eedce481a627560709074c84c8f8992b8a
Monique Parnis <monique.parnis@gmail.com>
2023-07-07 19:04:46 +0000

    Add challenge

--- arrow-functions.js
+++ arrow-functions.js
@@ -39,3 +39,57 @@ const returnMultipleLines = () => (
     </p>`
 )
 console.log(returnMultipleLines());
+
+// Challenge - Create a function using the arrow syntax
+// that should return the average marks of a subject.
+
+let students = [
+    {
+        name: 'John',
+        subjects: ['maths', 'english', 'cad'],
+        teacher: {maths: 'Harry', english: 'Joan', cad: 'Paul'},
+        results: {maths: 90, english: 75, cad: 87},
+    },
+    {
+        name: 'Emily',
+        subjects: ['science', 'maths','english', 'art'],
+        teacher: {science: 'Iris', maths: 'Harry', english: 'Joan', art: 'Simon'},
+        results: {science: 93, maths: 95, english: 80, art: 95},
+    },
+    {
+        name: 'Adam',
+        subjects: ['science', 'maths', 'art'],
+        teacher: {science: 'Iris', maths: 'Harry', art: 'Simon'},
+        results: {science: 63, maths: 79, art: 95},
+    }
+];
+
+// Declare a variable named averagePoints using the keyword const.
+// Assign it an arrow function, which should take two parameters named: arr and subject.
+const averagePoints = (arr, subject) => {
+    let subjectResults = 0;
+    let resultsCount = 0;
+
+    // Loop through the array that is passed in.
+    for (let i in arr) {
+        // check if the subject passed in is in said student's results object
+        const student = arr[i];
+        const studentResults = student["results"];
+
+        // if it is, add it to a cumulative total of all results for that subject
+        if (subject in studentResults) {
+            let hasSubject = studentResults[subject];
+        subjectResults += parseInt(hasSubject);
+        resultsCount += 1;
+        }
+    }
+    // return the average result for the subject
+    return subjectResults / resultsCount;
+}
+
+// Declare a variable named: averageMarks using the keyword let.
+// Assign it the return value from calling the function averagePoints,
+// and passing it the students array and a subject string
+let averageMarks = averagePoints(students, 'maths');
+// Log out the variable averageMarks to see its value
+console.log(averageMarks);
